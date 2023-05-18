8997b7f3c27da02935a35ffcc754c602dd76e1ae
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-18 17:10:32 -0300

    Criada stub de implementação de recurso de delete

--- src/main/java/com/flucas/libraryapi/api/resource/BookController.java
+++ src/main/java/com/flucas/libraryapi/api/resource/BookController.java
@@ -4,6 +4,7 @@ package com.flucas.libraryapi.api.resource;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,5 +59,11 @@ public class BookController {
         return service.getById(id)
                     .map(b -> modelMapper.map(b, BookDTO.class))
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+
     }
 }
