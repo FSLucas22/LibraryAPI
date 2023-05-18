bc822a06ca30ac19100448a8bf043b9f32349869
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-18 20:08:19 -0300

    Stub de update

--- src/main/java/com/flucas/libraryapi/api/resource/BookController.java
+++ src/main/java/com/flucas/libraryapi/api/resource/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -67,5 +68,11 @@ public class BookController {
         var book = service.getById(id)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         service.delete(book);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public BookDTO update(@PathVariable Long id, @RequestBody @Valid BookDTO dto) {
+        return modelMapper.map(service.getById(id).get(), BookDTO.class);
     }
 }
