986cc534bd61bee1be63dfcb7d14839716554928
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-22 21:41:42 -0300

    Teste de integração de findById do repository

--- src/test/java/com/flucas/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/flucas/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.flucas.libraryapi.model.repository;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,31 +26,45 @@ public class BookRepositoryTest {
     @Autowired
     BookRepository repository;
 
+    Book book;
+
+    @BeforeEach
+    public void setUp() {
+        book = Book.builder()
+            .title("Livro teste")
+            .author("Autor teste")
+            .isbn("123").build();
+    }
+
     @Test
     @DisplayName("Deve retornar verdadeiro quando existir um livro com o ISBN informado")
     public void shouldReturnTrueWhenIsbnExists() {
-        String isbn = "123";
-        Book book = Book.builder()
-            .title("Livro teste")
-            .author("Autor teste")
-            .isbn(isbn).build();
         entityManager.persist(book);
-
-        boolean exists = repository.existsByIsbn(isbn);
-        Assertions.assertThat(exists).isTrue();
+        Assertions.assertThat(repository.existsByIsbn(book.getIsbn())).isTrue();
     }
 
     @Test
     @DisplayName("Deve retornar falso quando não existir um livro com o ISBN informado")
     public void shouldReturnFalseWhenIsbnDoesntExists() {
-        String isbn = "123";
-        Book book = Book.builder()
-            .title("Livro teste")
-            .author("Autor teste")
-            .isbn(isbn).build();
         entityManager.persist(book);
+        Assertions.assertThat(repository.existsByIsbn("321")).isFalse();
+    }
 
-        boolean exists = repository.existsByIsbn("321");
-        Assertions.assertThat(exists).isFalse();
+    @Test
+    @DisplayName("Repository deve retornar o livro pelo id")
+    public void shouldFindBookById() {
+        entityManager.persist(book);
+        Optional<Book> foundBook = repository.findById(book.getId());
+        Assertions.assertThat(foundBook.isPresent()).isTrue();
+        Assertions.assertThat(foundBook.get().getId()).isEqualTo(book.getId());
+        Assertions.assertThat(foundBook.get().getTitle()).isEqualTo(book.getTitle());
+        Assertions.assertThat(foundBook.get().getAuthor()).isEqualTo(book.getAuthor());
+        Assertions.assertThat(foundBook.get().getIsbn()).isEqualTo(book.getIsbn());
+    }
+
+    @Test
+    @DisplayName("Repository deve retornar empty quando o id não existe")
+    public void shouldReturnEmptyWhenIdNotFound() {
+        Assertions.assertThat(repository.findById(2L).isEmpty()).isTrue();
     }
 }
