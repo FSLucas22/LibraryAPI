4cc4ba34addf321993c68ed6541745dda073a69d
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-23 20:55:43 -0300

    Testes de integração de save e delete

--- src/test/java/com/flucas/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/flucas/libraryapi/model/repository/BookRepositoryTest.java
@@ -30,7 +30,11 @@ public class BookRepositoryTest {
 
     @BeforeEach
     public void setUp() {
-        book = Book.builder()
+        book = createBook();
+    }
+
+    Book createBook() {
+        return Book.builder()
             .title("Livro teste")
             .author("Autor teste")
             .isbn("123").build();
@@ -66,5 +70,71 @@ public class BookRepositoryTest {
     @DisplayName("Repository deve retornar empty quando o id não existe")
     public void shouldReturnEmptyWhenIdNotFound() {
         Assertions.assertThat(repository.findById(2L).isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Repository deve salvar um livro")
+    public void shouldSaveNewBook() {
+        var savedBook = repository.save(book);
+        Assertions.assertThat(savedBook.getId()).isNotNull();
+        Assertions.assertThat(repository.findById(savedBook.getId())).isPresent();
+    }
+
+    @Test
+    @DisplayName("Repository deve atualizar um livro")
+    public void shouldUpdateBook() {
+        
+        entityManager.persist(book);
+        var updatingBook = createBook();
+        updatingBook.setId(book.getId());
+        updatingBook.setTitle("Outro titulo");
+        updatingBook.setAuthor("Outro autor");
+
+        var updatedBook = repository.save(updatingBook);
+
+        Assertions.assertThat(updatedBook.getId()).isEqualTo(updatingBook.getId());
+        Assertions.assertThat(updatedBook.getTitle()).isEqualTo(updatingBook.getTitle());
+        Assertions.assertThat(updatedBook.getAuthor()).isEqualTo(updatingBook.getAuthor());
+        Assertions.assertThat(updatedBook.getIsbn()).isEqualTo(updatingBook.getIsbn());
+    }
+
+    @Test
+    @DisplayName("Repository só deve atualizar o livro informado")
+    public void shouldNotUpdateDifferentBook() {
+        entityManager.persist(book);
+        var differentBook = createBook();
+        differentBook.setIsbn("321");
+        entityManager.persist(differentBook);
+
+        book.setTitle("Outro titulo");
+        book.setAuthor("Outro autor");
+        
+        repository.save(book);
+        var copyOfDifferentBook = repository.findById(differentBook.getId());
+        Assertions.assertThat(copyOfDifferentBook.isPresent()).isTrue();
+        Assertions.assertThat(copyOfDifferentBook.get().getId()).isEqualTo(differentBook.getId());
+        Assertions.assertThat(copyOfDifferentBook.get().getTitle()).isEqualTo(differentBook.getTitle());
+        Assertions.assertThat(copyOfDifferentBook.get().getAuthor()).isEqualTo(differentBook.getAuthor());
+        Assertions.assertThat(copyOfDifferentBook.get().getIsbn()).isEqualTo(differentBook.getIsbn());
+    }
+
+    @Test
+    @DisplayName("Repository deve excluir um livro")
+    public void shouldDeleteBook() {
+        entityManager.persist(book);
+        repository.delete(book);
+        Assertions.assertThat(repository.findById(book.getId()).isEmpty()).isTrue();
+        
+    }
+
+    @Test
+    @DisplayName("Repository somente deve excluir o livro informado")
+    public void shouldNotDeleteDifferentBook() {
+        var differentBook = createBook();
+        differentBook.setIsbn("321");
+        entityManager.persist(book);
+        entityManager.persist(differentBook);
+        repository.delete(book);
+        Assertions.assertThat(repository.findById(differentBook.getId()).isPresent()).isTrue();
     }
 }
