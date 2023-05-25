3aef714bba0d776c20b63701c1462c1320fc6343
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-24 22:39:04 -0300

    Primeiro teste para LoanController

--- src/test/java/com/flucas/libraryapi/api/resource/LoanControllerTest.java
+++ src/test/java/com/flucas/libraryapi/api/resource/LoanControllerTest.java
@@ -2,6 +2,9 @@ package com.flucas.libraryapi.api.resource;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +12,7 @@ import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -18,7 +22,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flucas.libraryapi.api.dto.LoanDTO;
 import com.flucas.libraryapi.model.entity.Book;
-import com.flucas.libraryapi.service.BookService;
+import com.flucas.libraryapi.model.entity.Loan;
+import com.flucas.libraryapi.service.interfaces.BookService;
+import com.flucas.libraryapi.service.interfaces.LoanService;
 
 
 @ExtendWith(SpringExtension.class)
@@ -27,12 +33,16 @@ import com.flucas.libraryapi.service.BookService;
 @WebMvcTest(LoanController.class)
 public class LoanControllerTest {
 
-    
+    static final String LOAN_API = "/api/loans";
+
     @Autowired
     MockMvc mvc;
 
     @MockBean
     BookService bookService;
+
+    @MockBean
+    LoanService loanService;
 
     @Test
     @DisplayName("Deve realizar um emprestimo")
@@ -45,7 +55,18 @@ public class LoanControllerTest {
                             .id(1L)
                             .isbn("isbn")
                             .build()));
+        
+        Loan loan = null;
 
-        var request = MockMvcRequestBuilders.post(json, null)
+        BDDMockito.given(loanService.save(any(Loan.class))).willReturn(loan);
+
+        var request = MockMvcRequestBuilders.post(LOAN_API)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json);
+
+        mvc
+            .perform(request)
+            .andExpect(status().isCreated());
     }
 }
