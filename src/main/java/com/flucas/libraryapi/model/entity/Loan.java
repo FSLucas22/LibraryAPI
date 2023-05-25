5289b1e1d6e78698c32735cebf1ee15e3a91149f
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-25 20:25:41 -0300

    Adicionada anotação para uso do ModelMapper

--- src/main/java/com/flucas/libraryapi/model/entity/Loan.java
+++ src/main/java/com/flucas/libraryapi/model/entity/Loan.java
@@ -3,10 +3,12 @@ package com.flucas.libraryapi.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Loan {
     private Long id;
     private String isbn;
