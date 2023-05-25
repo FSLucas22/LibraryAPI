f2f3a3c405f90ff886f62b7d75d872c732d52882
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-24 22:49:56 -0300

    Criado stub de mapping para post em LoanController

--- src/main/java/com/flucas/libraryapi/api/resource/LoanController.java
+++ src/main/java/com/flucas/libraryapi/api/resource/LoanController.java
@@ -1,5 +1,25 @@
 package com.flucas.libraryapi.api.resource;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flucas.libraryapi.api.dto.LoanDTO;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("api/loans")
+@RequiredArgsConstructor
 public class LoanController {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public LoanDTO create(@RequestBody LoanDTO dto) {
+        return null;
+    }
 }
