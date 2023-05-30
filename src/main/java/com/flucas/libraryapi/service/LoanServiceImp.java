bd6dd2d780d36407bf4bb8d981120f2eda715c9e
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-29 23:02:43 -0300

    Implementado save

--- src/main/java/com/flucas/libraryapi/service/LoanServiceImp.java
+++ src/main/java/com/flucas/libraryapi/service/LoanServiceImp.java
@@ -1,5 +1,6 @@
 package com.flucas.libraryapi.service;
 
+import com.flucas.libraryapi.exceptions.BusinessException;
 import com.flucas.libraryapi.model.entity.Loan;
 import com.flucas.libraryapi.model.repository.LoanRepository;
 
@@ -11,6 +12,9 @@ public class LoanServiceImp {
     private final LoanRepository repository;
 
     public Loan save(Loan loan) {
+        if (repository.existsByIsbn(loan.getIsbn())) {
+            throw new BusinessException("Book already loaned");
+        }
         return repository.save(loan);
     }
 }
