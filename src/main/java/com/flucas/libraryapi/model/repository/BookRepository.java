13708f00529abab6fb6bc82acaec8a1badd28e5c
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-29 21:45:05 -0300

    Adicionado método ao repository

--- src/main/java/com/flucas/libraryapi/model/repository/BookRepository.java
+++ src/main/java/com/flucas/libraryapi/model/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.flucas.libraryapi.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flucas.libraryapi.model.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
+
+    Optional<Book> findByIsbn(String isbn);
 }
