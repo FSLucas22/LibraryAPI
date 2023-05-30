438947653742eac611fe941db56f2c682682e2b2
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-29 22:26:48 -0300

    Iniciada implementação de LoanService

---
+++ src/test/java/com/flucas/libraryapi/service/LoanServiceImp.java
@@ -0,0 +1,15 @@
+package com.flucas.libraryapi.service;
+
+import com.flucas.libraryapi.model.entity.Loan;
+import com.flucas.libraryapi.model.repository.LoanRepository;
+
+public class LoanServiceImp {
+
+    public LoanServiceImp(LoanRepository repository) {
+    }
+
+    public Loan save(Loan loan) {
+        return null;
+    }
+
+}
