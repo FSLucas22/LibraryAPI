6cf687926804dd3aff6f0afb31765f1a963f3b43
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-29 22:26:29 -0300

    Criado LoanRepository

---
+++ src/main/java/com/flucas/libraryapi/model/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.flucas.libraryapi.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flucas.libraryapi.model.entity.Loan;
+
+public interface LoanRepository extends JpaRepository<Loan, Long>{
+
+}
