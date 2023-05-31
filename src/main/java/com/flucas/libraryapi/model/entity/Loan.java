a7c02b3bcc859ab7555b67ff2b7e11a0195c24c0
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-31 00:00:47 -0300

    Criado mapeamento para Loan

--- src/main/java/com/flucas/libraryapi/model/entity/Loan.java
+++ src/main/java/com/flucas/libraryapi/model/entity/Loan.java
@@ -1,5 +1,13 @@
 package com.flucas.libraryapi.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,11 +15,24 @@ import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 public class Loan {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JoinColumn(name="id_book")
+    @ManyToOne
     private Book book;
+
+    @Column(length = 255)
     private String customer;
+
+    @Column
     private Boolean returned;
 }
