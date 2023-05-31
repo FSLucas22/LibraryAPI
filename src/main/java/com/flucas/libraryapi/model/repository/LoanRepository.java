be522c64e86e65b4426417e65d2129d937e46460
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-05-31 00:01:04 -0300

    Implementada query

--- src/main/java/com/flucas/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/com/flucas/libraryapi/model/repository/LoanRepository.java
@@ -1,10 +1,14 @@
 package com.flucas.libraryapi.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.flucas.libraryapi.model.entity.Book;
 import com.flucas.libraryapi.model.entity.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Long>{
-    boolean existsByBookAndNotReturned(Book book);
+    @Query(value = "select count(*) > 0 from Loan " +
+        " where (returned is null or returned = false) and book = :book ") // returned is false não funciona para mim
+    boolean existsByBookAndNotReturned(@Param("book") Book book);
 }
