84dee9eebedfb155a5a45955acb6646b595a3545
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-06-01 19:58:35 -0300

    Criados métodos

--- src/main/java/com/flucas/libraryapi/service/LoanServiceImp.java
+++ src/main/java/com/flucas/libraryapi/service/LoanServiceImp.java
@@ -1,14 +1,17 @@
 package com.flucas.libraryapi.service;
 
+import java.util.Optional;
+
 import com.flucas.libraryapi.exceptions.BusinessException;
 import com.flucas.libraryapi.model.entity.Loan;
 import com.flucas.libraryapi.model.repository.LoanRepository;
+import com.flucas.libraryapi.service.interfaces.LoanService;
 
 import lombok.RequiredArgsConstructor;
 
 
 @RequiredArgsConstructor
-public class LoanServiceImp {
+public class LoanServiceImp implements LoanService {
     private final LoanRepository repository;
 
     public Loan save(Loan loan) {
@@ -16,5 +19,17 @@ public class LoanServiceImp {
             throw new BusinessException("Book already loaned");
         }
         return repository.save(loan);
+    }
+
+    @Override
+    public Optional<Loan> getById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public void update(Loan loan) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 }
--- src/main/java/com/flucas/libraryapi/service/interfaces/LoanService.java
+++ src/main/java/com/flucas/libraryapi/service/interfaces/LoanService.java
@@ -1,7 +1,13 @@
 package com.flucas.libraryapi.service.interfaces;
 
+import java.util.Optional;
+
 import com.flucas.libraryapi.model.entity.Loan;
 
 public interface LoanService {
     Loan save(Loan loan);
+
+    Optional<Loan> getById(Long id);
+
+    void update(Loan loan);
 }
