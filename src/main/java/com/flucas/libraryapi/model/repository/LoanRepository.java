f74d46de3ef60a619baa2d89319d3616f9011bba
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-06-01 23:21:45 -0300

    Adicionado método

--- src/main/java/com/flucas/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/com/flucas/libraryapi/model/repository/LoanRepository.java
@@ -1,5 +1,6 @@
 package com.flucas.libraryapi.model.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,6 @@ public interface LoanRepository extends JpaRepository<Loan, Long>{
     @Query(value = "select count(*) > 0 from Loan " +
         " where (returned is null or returned = false) and book = :book ") // returned is false não funciona para mim
     boolean existsByBookAndNotReturned(@Param("book") Book book);
+
+    Page<Loan> findByBookIsbnOrCustomer(String isbn, String customer);
 }
