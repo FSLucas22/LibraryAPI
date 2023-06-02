4e48a1e90496106ba5cec6ac7dd17b62dbe2069d
LucasFS22 <lucassantos@presconinformatica.com.br>
2023-06-01 23:31:45 -0300

    Otimizados imports

--- src/test/java/com/flucas/libraryapi/service/LoanServiceTest.java
+++ src/test/java/com/flucas/libraryapi/service/LoanServiceTest.java
@@ -1,7 +1,6 @@
 package com.flucas.libraryapi.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -13,10 +12,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
